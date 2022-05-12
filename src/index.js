779284657452d2e94afe384816da6a1209056d29
WellsSA <wel.cavzod@gmail.com>
2022-05-12 17:42:32 -0300

    feat(src): setting up utils and base definitions

--- src/index.js
+++ src/index.js
@@ -1,2 +1,16 @@
-// TODO: Dê uma olhada no projeto oficial do módulo 06 (Expressões Regulares - RegExp) para implementar este arquivo.
-// Dica do wells: no lugar da chamada do PDF parser, um simples `.toString()` resolve, já que nós é que implementaremos o "CSV parser"
+'use strict';
+
+const { readFile } = require('fs/promises');
+const { join } = require('path');
+
+const TextProcessorFacade = require('./textProcessorFacade');
+
+(async () => {
+  const filePath = join(__dirname, './../docs/projeto-de-lei.csv');
+  const dataBuffer = await readFile(join(__dirname, filePath));
+  const data = dataBuffer.toString();
+
+  const instance = new TextProcessorFacade(data);
+  const projects = instance.getProjectsFromCSV();
+  console.log('projects', projects);
+})();
