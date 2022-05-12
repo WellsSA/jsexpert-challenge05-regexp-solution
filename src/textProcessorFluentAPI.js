fe2a22ac5a8a0bc4bbfb31dda782374fe91e0238
WellsSA <wel.cavzod@gmail.com>
2022-05-12 17:43:43 -0300

    feat(): setting up textProcessorFluentAPI

--- src/textProcessorFluentAPI.js
+++ src/textProcessorFluentAPI.js
@@ -1,1 +1,72 @@
-// TODO: Dê uma olhada no projeto oficial do módulo 06 (Expressões Regulares - RegExp) para implementar este arquivo.
+const { evaluateRegex } = require('./util');
+const Project = require('./project');
+
+class TextProcessorFluentAPI {
+  #content;
+  constructor(content) {
+    this.#content = content;
+  }
+  extractHeaders() {
+    const matchHeaders = evaluateRegex(/(?:(?!\n).)*/);
+    // to not include the \n, thats also an option => (?:(?!\n).)*
+
+    const [headers] = this.#content.match(matchHeaders);
+
+    this.#content = {
+      headers: headers,
+      content: this.#content,
+    };
+
+    return this;
+  }
+
+  extractContent() {
+    const { content } = this.#content;
+
+    const matchContent = evaluateRegex(/(?<=\n).*/g);
+
+    const projects = content.match(matchContent);
+    this.#content.content = projects;
+
+    return this;
+  }
+
+  splitValues() {
+    const { headers, content } = this.#content;
+
+    const matchSeparators = evaluateRegex(/;/);
+
+    this.#content = {
+      headers: headers.split(matchSeparators).filter(value => !!value),
+      content: content.map(project =>
+        project.split(matchSeparators).filter(value => !!value)
+      ),
+    };
+
+    return this;
+  }
+
+  mapRawObjects() {
+    const { headers, content } = this.#content;
+
+    this.#content = content.map(projectArr => {
+      return projectArr.reduce((finalObject, field, index) => {
+        finalObject[headers[index]] = field;
+        return finalObject;
+      }, {});
+    });
+
+    return this;
+  }
+
+  mapProjects() {
+    this.#content = this.#content.map(rawProject => new Project(rawProject));
+    return this;
+  }
+
+  build() {
+    return this.#content;
+  }
+}
+
+module.exports = TextProcessorFluentAPI;
--- test/textProcessorFluentAPI.test.js
+++ test/textProcessorFluentAPI.test.js
@@ -1,1 +1,187 @@
-// TODO: Dê uma olhada no projeto oficial do módulo 06 (Expressões Regulares - RegExp) para implementar este arquivo.
+const { describe, it } = require('mocha');
+const { expect } = require('chai');
+const TextProcessorFluentAPI = require('./../src/textProcessorFluentAPI');
+const mock = require('./mock/valid');
+
+describe('TextProcessorAPI', () => {
+  it('#build', () => {
+    const result = new TextProcessorFluentAPI(mock).build();
+    expect(result).to.be.deep.equal(mock);
+  });
+
+  it('#extractHeaders', () => {
+    const result = new TextProcessorFluentAPI(mock).extractHeaders().build();
+
+    const expected = {
+      headers: 'título;link;autor;etapa;ementa;indexadoresnorma;',
+      content: mock,
+    };
+    expect(result).to.be.deep.equal(expected);
+  });
+
+  it('#extractContent', () => {
+    const result = new TextProcessorFluentAPI(mock)
+      .extractHeaders()
+      .extractContent()
+      .build();
+
+    const expected = {
+      headers: 'título;link;autor;etapa;ementa;indexadoresnorma;',
+      content: [
+        'Projeto de lei 584/2016;http://www.al.sp.gov.br/propositura?id=1322563;Jorge Wilson Xerife do Consumidor;PAUTA;Dispõe sobre a inclusão de cláusula nos contratos de adesão aos serviços de telefonia fixa, de telefonia móvel e de banda larga móvel, e dá outras providências.;CONTRATO, OBRIGATORIEDADE, CLÁUSULA, SERVIÇO, TELEFONIA MÓVEL, TELEFONIA FIXA, PRAZO, INCLUSÃO, RESCISÃO CONTRATUAL, LIBERAÇÃO;',
+        'Projeto de lei 580/2016;http://www.al.sp.gov.br/propositura?id=1323286;Marcia Lia;PAUTA;Estabelece normas gerais para a realização de Concurso Público pela Administração Pública Direta e Indireta do Estado.;NORMAS, REALIZAÇÃO, CONCURSO PÚBLICO ESTADUAL, ESTADO DE SÃO PAULO, ADMINISTRAÇÃO PÚBLICA DIRETA E INDIRETA;',
+        'Projeto de lei 545/2016;http://www.al.sp.gov.br/propositura?id=1322832;Roberto Morais, Itamar Borges;PAUTA;Altera a Lei nº 13.550, de 2009, que dispõe sobre a utilização e proteção da vegetação nativa do Bioma Cerrado no Estado de São Paulo.;',
+      ],
+    };
+    expect(result).to.be.deep.equal(expected);
+  });
+
+  it('#splitValues', () => {
+    const result = new TextProcessorFluentAPI(mock)
+      .extractHeaders()
+      .extractContent()
+      .splitValues()
+      .build();
+
+    const expected = {
+      headers: [
+        'título',
+        'link',
+        'autor',
+        'etapa',
+        'ementa',
+        'indexadoresnorma',
+      ],
+      content: [
+        [
+          'Projeto de lei 584/2016',
+          'http://www.al.sp.gov.br/propositura?id=1322563',
+          'Jorge Wilson Xerife do Consumidor',
+          'PAUTA',
+          'Dispõe sobre a inclusão de cláusula nos contratos de adesão aos serviços de telefonia fixa, de telefonia móvel e de banda larga móvel, e dá outras providências.',
+          'CONTRATO, OBRIGATORIEDADE, CLÁUSULA, SERVIÇO, TELEFONIA MÓVEL, TELEFONIA FIXA, PRAZO, INCLUSÃO, RESCISÃO CONTRATUAL, LIBERAÇÃO',
+        ],
+        [
+          'Projeto de lei 580/2016',
+          'http://www.al.sp.gov.br/propositura?id=1323286',
+          'Marcia Lia',
+          'PAUTA',
+          'Estabelece normas gerais para a realização de Concurso Público pela Administração Pública Direta e Indireta do Estado.',
+          'NORMAS, REALIZAÇÃO, CONCURSO PÚBLICO ESTADUAL, ESTADO DE SÃO PAULO, ADMINISTRAÇÃO PÚBLICA DIRETA E INDIRETA',
+        ],
+        [
+          'Projeto de lei 545/2016',
+          'http://www.al.sp.gov.br/propositura?id=1322832',
+          'Roberto Morais, Itamar Borges',
+          'PAUTA',
+          'Altera a Lei nº 13.550, de 2009, que dispõe sobre a utilização e proteção da vegetação nativa do Bioma Cerrado no Estado de São Paulo.',
+        ],
+      ],
+    };
+    expect(result).to.be.deep.equal(expected);
+  });
+
+  it('#mapRawObjects', () => {
+    const result = new TextProcessorFluentAPI(mock)
+      .extractHeaders()
+      .extractContent()
+      .splitValues()
+      .mapRawObjects()
+      .build();
+
+    const expected = [
+      {
+        título: 'Projeto de lei 584/2016',
+        link: 'http://www.al.sp.gov.br/propositura?id=1322563',
+        autor: 'Jorge Wilson Xerife do Consumidor',
+        etapa: 'PAUTA',
+        ementa:
+          'Dispõe sobre a inclusão de cláusula nos contratos de adesão aos serviços de telefonia fixa, de telefonia móvel e de banda larga móvel, e dá outras providências.',
+        indexadoresnorma:
+          'CONTRATO, OBRIGATORIEDADE, CLÁUSULA, SERVIÇO, TELEFONIA MÓVEL, TELEFONIA FIXA, PRAZO, INCLUSÃO, RESCISÃO CONTRATUAL, LIBERAÇÃO',
+      },
+      {
+        título: 'Projeto de lei 580/2016',
+        link: 'http://www.al.sp.gov.br/propositura?id=1323286',
+        autor: 'Marcia Lia',
+        etapa: 'PAUTA',
+        ementa:
+          'Estabelece normas gerais para a realização de Concurso Público pela Administração Pública Direta e Indireta do Estado.',
+        indexadoresnorma:
+          'NORMAS, REALIZAÇÃO, CONCURSO PÚBLICO ESTADUAL, ESTADO DE SÃO PAULO, ADMINISTRAÇÃO PÚBLICA DIRETA E INDIRETA',
+      },
+      {
+        título: 'Projeto de lei 545/2016',
+        link: 'http://www.al.sp.gov.br/propositura?id=1322832',
+        autor: 'Roberto Morais, Itamar Borges',
+        etapa: 'PAUTA',
+        ementa:
+          'Altera a Lei nº 13.550, de 2009, que dispõe sobre a utilização e proteção da vegetação nativa do Bioma Cerrado no Estado de São Paulo.',
+      },
+    ];
+    expect(result).to.be.deep.equal(expected);
+  });
+
+  it('#mapProjects', () => {
+    const result = new TextProcessorFluentAPI(mock)
+      .extractHeaders()
+      .extractContent()
+      .splitValues()
+      .mapRawObjects()
+      .mapProjects()
+      .build();
+
+    const expected = [
+      {
+        id: '1322563',
+        numero: '584',
+        ano: '2016',
+        autores: [
+          {
+            nome: 'Jorge Consumidor',
+          },
+        ],
+        url: 'http://www.al.sp.gov.br/propositura?id=1322563',
+        indexadores: [
+          'CONTRATO',
+          'OBRIGATORIEDADE',
+          'CLÁUSULA',
+          'SERVIÇO',
+          'TELEFONIA MÓVEL',
+          'TELEFONIA FIXA',
+          'PRAZO',
+          'INCLUSÃO',
+          'RESCISÃO CONTRATUAL',
+          'LIBERAÇÃO',
+        ],
+      },
+      {
+        id: '1323286',
+        numero: '580',
+        ano: '2016',
+        autores: [
+          {
+            nome: 'Marcia Lia',
+          },
+        ],
+        url: 'http://www.al.sp.gov.br/propositura?id=1323286',
+        indexadores: [
+          'NORMAS',
+          'REALIZAÇÃO',
+          'CONCURSO PÚBLICO ESTADUAL',
+          'ESTADO DE SÃO PAULO',
+          'ADMINISTRAÇÃO PÚBLICA DIRETA E INDIRETA',
+        ],
+      },
+      {
+        id: '1322832',
+        numero: '545',
+        ano: '2016',
+        autores: [{ nome: 'Roberto Morais' }, { nome: 'Itamar Borges' }],
+        url: 'http://www.al.sp.gov.br/propositura?id=1322832',
+        indexadores: [],
+      },
+    ];
+    expect(result).to.be.deep.equal(expected);
+  });
+});
